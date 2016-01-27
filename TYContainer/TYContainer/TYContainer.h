6f59c5d0a15010ba95c1936f4556c5f59f998018
troy <siren.lb@alibaba-inc.com>
2016-01-27 13:56:52 +0800

    Ìadd TYContain

---
+++ TYContainer/TYContainer/TYContainer.h
@@ -0,0 +1,19 @@
+//
+//  TYContainer.h
+//  TYContainer
+//
+//  Created by 李彬 on 16/1/27.
+//  Copyright © 2016年 李彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TYContainer.
+FOUNDATION_EXPORT double TYContainerVersionNumber;
+
+//! Project version string for TYContainer.
+FOUNDATION_EXPORT const unsigned char TYContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TYContainer/PublicHeader.h>
+
+
